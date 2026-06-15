dcad1e3fb6a8495c6f49bb360dd9beda05d1ca38
pr0tonx <felipepeengler@outlook.com>
2026-06-15 00:01:26 -0300

    testing sonarcloud

--- LogicaPura/src/tests/basic.test.js
+++ LogicaPura/src/tests/basic.test.js
@@ -4,4 +4,8 @@ describe('basic test', () => {
     it('should pass', () => {
         expect(1 + 1).toBe(2)
     })
+
+    it('should pass as well', () => {
+        expect(1 + 1).toBe(2)
+    })
 })
